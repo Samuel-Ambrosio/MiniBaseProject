8446558124ec0884c5fa1ecd0345d343ad2e3aa5
Samuel-Ambrosio <samuel.ambrosiovazquez@gmail.com>
2021-06-13 11:34:10 +0200

    Update libraries

--- app/src/main/java/com/ghrisav/minibaseproject/features/home/di/FeatureHomeModule.kt
+++ app/src/main/java/com/ghrisav/minibaseproject/features/home/di/FeatureHomeModule.kt
@@ -1,7 +1,7 @@
 package com.ghrisav.minibaseproject.features.home.di
 
 import com.ghrisav.minibaseproject.features.home.ui.viewmodel.HomeViewModel
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val featureHomeModule = module {
